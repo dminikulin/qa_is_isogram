fa27c3479ffc4edcadd5ec4889ec10dee723f26f
Dmytro Nikulin <dmytronikulin@Dmytros-MacBook-Air.local>
2026-02-27 11:24:13 +0200

    Add solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,24 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true for empty string`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should return true
+    if all the letters in the string are different`, () => {
+    expect(isIsogram('polygraphic')).toBe(true);
+  });
+
+  it(`should return false
+    if any of the letters in the string repeat`, () => {
+    expect(isIsogram('whoops')).toBe(false);
+  });
+
+  it(`should return false
+    if any of the letters in the string repeat
+    while ignoring the letter case`, () => {
+    expect(isIsogram('Analogy')).toBe(false);
+  });
 });
